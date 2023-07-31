c0fc636b9638ca02be5d8490b32ea9bd4f2c1981
Ian Quinn <ian_c_quinn@progressive.com>
2023-07-31 12:36:53 -0400

    before lunch monday

---
+++ EmployeesHrSolution/EmployeesHrApi/Controllers/EmployeesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesHrApi.Controllers
+{
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet("/employees")]
+        public async Task<ActionResult> GetEmployeesAsync()
+        {
+            return Ok();
+        }
+    }
+}
