d773a3b86e204960f6a3947b0595e85cfc1d9a85
Ian Quinn <ian_c_quinn@progressive.com>
2023-08-02 12:36:47 -0400

    initial commit before lunch monday

--- EmployeesHrSolution/EmployeesHrApi/Controllers/HiringRequestController.cs
+++ EmployeesHrSolution/EmployeesHrApi/Controllers/HiringRequestController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using EmployeesHrApi.Data;
 using EmployeesHrApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeesHrApi.Controllers
 {
@@ -9,11 +11,39 @@ namespace EmployeesHrApi.Controllers
     {
         private readonly EmployeeDataContext _context;
         private readonly IMapper _mapper;
+        private readonly MapperConfiguration _mapperConfig;
 
-        public HiringRequestController(EmployeeDataContext context, IMapper mapper)
+
+        public HiringRequestController(EmployeeDataContext context, IMapper mapper, MapperConfiguration config)
         {
             _context = context;
             _mapper = mapper;
+            _mapperConfig = config;
+        }
+
+        [HttpPost("/denied-hiring-requests")]
+        public async Task<ActionResult> DenyHiringRequestAsync([FromBody] HiringRequestResponseModel request)
+        {
+            var id = int.Parse(request.Id);
+            if (request.Status != HiringRequestStatus.WaitingForJobAssignment)
+            {
+                return BadRequest("Can only deny pending assignments");
+            }
+            var savedHiringRequest = await _context.HiringRequests.Where(h => h.Id == id)
+                .SingleOrDefaultAsync();
+
+
+
+            if (savedHiringRequest == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                savedHiringRequest.Status = HiringRequestStatus.Denied;
+                await _context.SaveChangesAsync();
+                return NoContent(); // or the mapped hiring request.
+            }
         }
 
         [HttpPost("/hiring-requests")]
@@ -32,7 +62,25 @@ namespace EmployeesHrApi.Controllers
             //   - Add Header "Location" - with the Url of the new resource.
             //   - Return them a copy of the new resource
             var response = _mapper.Map<HiringRequestResponseModel>(newHiringRequest);
-            return Ok(response);
+            return CreatedAtRoute("hiring-request#gethiringrequestbyidasync", new { id = response.Id }, response);
+        }
+
+        [HttpGet("/hiring-requests/{id:int}", Name="hiring-request#gethiringrequestbyidasync")]
+        public async Task<ActionResult> GetHiringRequestByIdAsync(int id)
+        {
+            var hiringRequest = await _context.HiringRequests
+                .Where(e => e.Id == id)
+                .ProjectTo<HiringRequestResponseModel>(_mapperConfig)
+                .SingleOrDefaultAsync();
+
+            if (hiringRequest is not null)
+            {
+                return Ok(hiringRequest);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
--- EmployeesHrSolution/EmployeesHrApi/Data/EmployeeDataContext.cs
+++ EmployeesHrSolution/EmployeesHrApi/Data/EmployeeDataContext.cs
@@ -14,6 +14,19 @@ public class EmployeeDataContext : DbContext
     }
     public DbSet<Employee> Employees { get; set; }
     public DbSet<HiringRequests> HiringRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>()
+             .Property(e => e.Email).HasMaxLength(200);
+
+        modelBuilder.Entity<Employee>().Property(e => e.FirstName).HasMaxLength(200);
+        modelBuilder.Entity<Employee>().Property(e => e.LastName).HasMaxLength(200);
+        modelBuilder.Entity<Employee>().HasIndex(e => e.Department).IsClustered(false);
+        modelBuilder.Entity<Employee>().Property(e => e.Salary).HasPrecision(16, 2);
+        modelBuilder.Entity<HiringRequests>().Property(hr => hr.RequiredSalary).HasPrecision(16, 2);
+    }
+
     public IQueryable<Employee> GetEmployeesByDepartment(string department)
     {
         if (department != "All")
